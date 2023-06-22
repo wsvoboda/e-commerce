5d6126f636618a595b8ccaa51ba8200534bcf2d4
Whitney Svoboda <whitney.svoboda@gmail.com>
2023-06-22 17:41:16 -0600

    update basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={`${process.env.BASE_URL}`}>
+      <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
           <App />
       </BrowserRouter>
     </Provider>
